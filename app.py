04896b27ceba7eb76b1ad5b91a43b6570908505b
unknown <travisbostick@gmail.com>
2020-11-28 21:12:50 -0600

    minor changes and notes

--- app.py
+++ app.py
@@ -7,9 +7,14 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
+
+############## review class notes on session.close to make sure this is set up right
+
+
+
 ## set up engine and connection, not sure if connection is needed
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
-conn = engine.connect()
+# conn = engine.connect()
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -17,7 +22,7 @@ Base = automap_base()
 Base.prepare(engine,reflect=True)
 
 ##### ---- not sure if I need this needed
-Base.metadata.create_all(conn)
+# Base.metadata.create_all(conn)
 
 
 # Save references to each table
@@ -28,9 +33,14 @@ st = Base.classes.station
 session=Session(engine)
 
 
-
 ###create the app
 app = Flask(__name__)
+
+### gives the date range to use for a given station number in the "first_date,last_date" format
+def first_last_date(station_num):
+    last_date = dt.datetime.strptime(session.query(mt.date,mt.tobs).filter(mt.station==station_num).order_by(mt.date.desc()).all()[0][0],'%Y-%m-%d')
+    first_date = last_date - dt.timedelta(days=365)
+    return(first_date,last_date)
 
 
 ### to be used with the home page
@@ -48,6 +58,8 @@ def home():
     return jsonify(route_list)
 
 
+
+
 #### query to get the date and preciptation for 
 prec=dict(engine.execute('SELECT date,prcp FROM measurement').fetchall())
 ######## defining the precipitation page
@@ -56,6 +68,8 @@ prec=dict(engine.execute('SELECT date,prcp FROM measurement').fetchall())
 def precipitation():
     print("sucessfully reach the precipitation page")
     return jsonify(prec)
+
+
 
 #### gets the station id and name  for the stations page
 station=dict(engine.execute("SELECT station,name FROM station").fetchall())
@@ -70,16 +84,18 @@ def stations():
 top_station_id = "USC00519281"
 
 ### find the first and last date we'll be using for the temperature page query
-last_date = dt.datetime.strptime(session.query(mt.date,mt.tobs).filter(mt.station==top_station_id).order_by(mt.date.desc()).all()[0][0],'%Y-%m-%d')
-first_date = last_date - dt.timedelta(days=365)
+top_first_date, top_last_date = first_last_date(top_station_id)
 
 ### query to get the last year of data for the top station
-temp_page_result = dict(session.query(mt.date,mt.tobs).filter(mt.station==top_station_id).all())
+temp_page_result = dict(session.query(mt.date,mt.tobs).filter(mt.station==top_station_id,mt.date>top_first_date).all())
 
 @app.route("/api/v1.0/tobs")
 def temperature():
     print("sucessfully reached the temperature page")
     return jsonify(temp_page_result)
+
+
+
 
 
 # @app.route("/api/v1.0/<start>")
